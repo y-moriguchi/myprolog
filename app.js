b4966936b41e19d0932b3ebbad4548707b9415bf
y-moriguchi <daikanyamayuiichi@gmail.com>
2019-10-22 11:44:53 +0900

    add command line option

--- app.js
+++ app.js
@@ -7,57 +7,154 @@
  * http://opensource.org/licenses/mit-license.php
  **/
 var Prolog = require("./myprolog.js"),
-	readlineSync = require("readline-sync"),
-	prolog = Prolog.makeProlog(),
-	lineInput,
-	nowPrompt,
-	input,
-	cont;
+    fs = require("fs"),
+    readlineSync = require("readline-sync"),
+    prolog = Prolog.makeProlog(),
+    defaultSuccess = createSuccess(function() {
+        return readlineSync.keyIn();
+    });
 
 function fail() {
-	console.log("false.");
+    console.log("false.");
 }
 
-function success(frame, fail) {
-	var boundVariables = Prolog.getAllBoundVariablesToString(frame),
-		variableArray = [],
-		i,
-		count,
-		comma,
-		action;
-	for(i in boundVariables) {
-		if(boundVariables.hasOwnProperty(i)) {
-			variableArray.push(i + " = " + boundVariables[i]);
-		}
-	}
-	for(i = 0; i < variableArray.length; i++) {
-		comma = i < variableArray.length - 1 ? ",\n" : "";
-		process.stdout.write(variableArray[i] + comma);
-	}
-	action = readlineSync.keyIn();
-	if(action === ";") {
-		fail();
-	}
+function createSuccess(keyIn) {
+    return function(frame, fail) {
+        var boundVariables = Prolog.getAllBoundVariablesToString(frame),
+            variableArray = [],
+            i,
+            count,
+            comma,
+            action;
+
+        for(i in boundVariables) {
+            if(boundVariables.hasOwnProperty(i)) {
+                variableArray.push(i + " = " + boundVariables[i]);
+            }
+        }
+        for(i = 0; i < variableArray.length; i++) {
+            comma = i < variableArray.length - 1 ? ",\n" : "";
+            process.stdout.write(variableArray[i] + comma);
+        }
+        action = keyIn();
+        if(action === ";") {
+            fail();
+        }
+    };
 }
 
-while(true) {
-	input = "";
-	nowPrompt = "> ";
-	while(true) {
-		lineInput = readlineSync.prompt({ prompt: nowPrompt }).replace(/^\s+|\s+$/g, '');
-		if(lineInput.substr(-1) === ".") {
-			input += lineInput.substring(0, lineInput.length - 1);
-			break;
-		} else {
-			input += lineInput + " ";
-			nowPrompt = ">>";
-		}
-	}
-	if(input === "halt") {
-		break;
-	}
-	cont = prolog.execute(input, fail);
-	if(cont) {
-		cont(success, fail);
-	}
+function readProgram(success, handler) {
+    var input,
+        nowPrompt,
+        lineInput,
+        cont;
+
+    while(true) {
+        input = "";
+        nowPrompt = "> ";
+        while(true) {
+            lineInput = handler({ prompt: nowPrompt });
+            if(!lineInput && lineInput !== "") {
+                return;
+            }
+            lineInput = lineInput.replace(/^\s+|\s+$/g, '');
+            if(lineInput.substr(-1) === ".") {
+                input += lineInput.substring(0, lineInput.length - 1);
+                break;
+            } else {
+                input += lineInput + " ";
+                nowPrompt = ">>";
+            }
+        }
+        if(input === "halt") {
+            break;
+        }
+        cont = prolog.execute(input, fail);
+        if(cont) {
+            cont(success, fail);
+        }
+    }
 }
+
+function isFile(file) {
+    try {
+        fs.statSync(file);
+        return true;
+    } catch(e) {
+        return false;
+    }
+}
+
+function readFile(file) {
+    var fileString;
+
+    if(isFile(file)) {
+        fileString = fs.readFileSync(file, "utf8");
+        return fileString;
+    } else {
+        console.log("File not found: " + file);
+        process.exit(2);
+    }
+}
+
+function readPrompt(success) {
+    readProgram(success, function(option) {
+        return readlineSync.prompt(option);
+    });
+}
+
+function readFromFile(success, file, echo) {
+    var program = readFile(file),
+        lines = program.split(/\r\n|\n|\r/),
+        count = 0;
+
+    readProgram(success, function(option) {
+        if(count < lines.length) {
+            if(echo && lines[count] !== "") {
+                console.log(lines[count]);
+            }
+            return lines[count++];
+        } else {
+            return null;
+        }
+    });
+}
+
+function main() {
+    var success,
+        argvFlag = 3,
+        flagString,
+        interactive = false,
+        echo = true;
+
+    success = defaultSuccess;
+    for(; argvFlag < process.argv.length; argvFlag++) {
+        flagString = process.argv[argvFlag - 1];
+        if(flagString === "-i") {
+            interactive = true;
+        } else if(flagString === "-f") {
+            success = createSuccess(function() {
+                console.log(";");
+                return ";";
+            });
+        } else if(flagString === "-s") {
+            echo = false;
+        } else if(/^-/.test(flagString)) {
+            console.log("Invalid flag: " + flagString);
+            process.exit(2);
+        } else {
+            break;
+        }
+    }
+    if(process.argv.length < argvFlag) {
+        readPrompt(defaultSuccess);
+    } else if(process.argv.length === argvFlag) {
+        readFromFile(success, process.argv[argvFlag - 1], echo);
+        if(interactive) {
+            readPrompt(success);
+        }
+    }
+}
+
+main();
+
